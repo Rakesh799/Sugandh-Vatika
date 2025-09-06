00cdf9ce3e292060645165d0f10bf2aaec190944
Rakesh Choudhury <rakeshchoudhury074@gmail.com>
2025-09-06 21:18:40 +0530

    basic setup done

---
+++ src/app/page.jsx
@@ -0,0 +1,50 @@
+
+export default function Home() {
+  return (
+    <div>
+
+      <ul>
+        <li><b>Traditional & Spiritual</b>
+          <ul>
+            <li>SugandhVatika.com (Garden of fragrance)</li>
+            <li>DivyaSugandh.com (Divine fragrance)</li>
+            <li>PoojaSugandh.com</li>
+            <li>AaradhyaFragrances.com</li>
+            <li>SatvikSugandh.com</li>
+          </ul>
+        </li>
+
+        <li><b>Short & Catchy</b>
+          <ul>
+            <li>DhoopAura.com</li>
+            <li>IncenseJoy.com</li>
+            <li>FragranceNest.com</li>
+            <li>PureAroma.in</li>
+            <li>AromaRoots.com</li>
+          </ul>
+        </li>
+
+        <li><b>Premium & Elegant</b>
+          <ul>
+            <li>SacredScents.in</li>
+            <li>MysticAroma.com</li>
+            <li>VedikaFragrances.com</li>
+            <li>AnantSugandh.com (Infinite fragrance)</li>
+            <li>EternalIncense.com</li>
+          </ul>
+        </li>
+
+        <li><b>Desi Touch</b>
+          <ul>
+            <li>SugandhBazar.com</li>
+            <li>DhoopMahal.com</li>
+            <li>AgarbattiGhar.com</li>
+            <li>SugandhSanskriti.com</li>
+            <li>BhaktiSugandh.com</li>
+          </ul>
+        </li>
+      </ul>
+
+    </div>
+  );
+}
