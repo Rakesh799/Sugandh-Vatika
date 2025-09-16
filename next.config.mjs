7f61423b90e0de640a2d9c59b198ea35a8c365a9
Rakesh Choudhury <rakeshchoudhury074@gmail.com>
2025-09-16 11:25:34 +0530

    added all security headers.

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,24 @@ const securityHeaders = [
       key: "Content-Security-Policy",
       value: "default-src 'self' https://satvik-sugandh.vercel.app; img-src 'self' data: https://satvik-sugandh.vercel.app; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://satvik-sugandh.vercel.app; style-src 'self' 'unsafe-inline' https://satvik-sugandh.vercel.app; font-src 'self' data: https://satvik-sugandh.vercel.app; connect-src 'self' https://satvik-sugandh.vercel.app;",
     },
+    {
+        key: "X-Frame-Options",
+        value: "DENY", // Prevents clickjacking
+      },
+      {
+        key: "X-Content-Type-Options",
+        value: "nosniff", // Blocks MIME sniffing
+      },
+      {
+        key: "Referrer-Policy",
+        value: "strict-origin-when-cross-origin", 
+        // Safe balance between privacy + analytics
+      },
+      {
+        key: "Permissions-Policy",
+        value: "camera=(), microphone=(), geolocation=()", 
+        // Disables these APIs unless you explicitly need them
+      },
   ];
 const nextConfig = {
 
