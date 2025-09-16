2ac9c85116c2bf459a0266bdc1d119847c662103
Rakesh Choudhury <rakeshchoudhury074@gmail.com>
2025-09-16 11:04:21 +0530

    added csp in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const securityHeaders = [
+    {
+      key: "Content-Security-Policy",
+      value: "default-src 'self' https://satvik-sugandh.vercel.app; img-src 'self' data: https://satvik-sugandh.vercel.app; script-src 'self' https://satvik-sugandh.vercel.app; style-src 'self' https://satvik-sugandh.vercel.app; font-src 'self' data: https://satvik-sugandh.vercel.app; connect-src 'self' https://satvik-sugandh.vercel.app;",
+    },
+  ];
+const nextConfig = {
+
+    async headers() {
+        return [
+          {
+            source: "/(.*)", // apply to all routes
+            headers: securityHeaders,
+          },
+        ];
+      },
+};
 
 export default nextConfig;
