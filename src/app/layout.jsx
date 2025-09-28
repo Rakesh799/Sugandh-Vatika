c9b48dea4d240603590fde7f445e91e18b82efc9
Rakesh Choudhury <rakeshchoudhury074@gmail.com>
2025-09-28 15:41:02 +0530

    added google search console

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,6 +6,9 @@ import Footer from "@/components/layout/Footer";
 export const metadata = {
   title: "SatvikSugandh",
   description: "Discover divine fragrances at SatvikSugandh - your trusted online store for premium dhoop, agarbatti, and natural incense sticks. Perfect for pooja, meditation, and creating a peaceful ambiance. Shop pure aromas today!",
+  verification: {
+    google: "google-site-verification=t5_rwvubrQGzJVOHwhAFGq7IOhvxahE4C3-SLpc3uFk", 
+  },
   icons: {
     icon: [
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
