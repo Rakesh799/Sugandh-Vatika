98db63f2df8338ac6f488a740f3f42a778d3eb59
Rakesh Choudhury <rakeshchoudhury074@gmail.com>
2025-10-13 19:57:59 +0530

    added song and dynamic route

--- src/app/songs/page.jsx
+++ src/app/songs/page.jsx
@@ -1,14 +1,18 @@
-import GaneshStotra from '@/components/songs/GaneshStotra'
-import ShivaTandavStotram from '@/components/songs/ShivaTandavStotram'
-import React from 'react'
+import Link from 'next/link';
+import songsData from '@/data/songs.json';
 
-function Songs() {
-    return (
-        <div>
-            <ShivaTandavStotram />
-            <GaneshStotra />
-        </div>
-    )
+export default function SongsList() {
+  return (
+    <div className="p-4 max-w-3xl mx-auto space-y-2">
+      {songsData.map((song) => (
+        <Link
+          key={song.id}
+          href={`/songs/${song.slug}`}
+          className="block text-blue-600 hover:underline"
+        >
+          {song.title}
+        </Link>
+      ))}
+    </div>
+  );
 }
-
-export default Songs
---
+++ src/components/songs/Breathless.jsx
@@ -0,0 +1,125 @@
+export default function Breathless() {
+    return (
+      <div className="space-y-4 p-4 max-w-3xl mx-auto leading-relaxed text-center">
+        <h2 className="text-2xl font-bold text-center mb-6">Koi Jo Mila To Mujhe Aisa Lagta Tha</h2>
+  
+        <p>
+          Koi jo mila to mujhe aisa lagta tha <br />
+          Jaise meri saari duniya main geeton ki rut <br />
+          Aur rangon ki barkha hai, khushbu ki aandhi hai <br />
+          Mehki hui si ab saari fizaayen hain <br />
+          Behki hui si ab saari hawaayen hain <br />
+          Khoyi hui si ab saari dishaayen hain <br />
+          Badli hui se ab saari adaayen hain
+        </p>
+  
+        <p>
+          Jaagi ummengein hain, dhadak raha hai dil <br />
+          Sanson main toofaan hain, hoton pe nagme hain <br />
+          Aakhon main sapne hain, sapnon main beete hue <br />
+          Saare vo lamhe hain
+        </p>
+  
+        <p>
+          Jab koi aaya tha, nazron pe chhaya tha <br />
+          Dil main samaya tha, kaise main bataun tumhe <br />
+          Kaise use paaya tha
+        </p>
+  
+        <p>
+          Pyaar ke chehre pe bikhri jo julphen <br />
+          To aisa lagta tha jaise kohre ke peechhe <br />
+          Ek os main dhula hua phool khila hai jaise <br />
+          Badal main ik chaand chhupa hai <br />
+          Aur jhaank raha hai jaise
+        </p>
+  
+        <p>
+          Raat ke parde main ek savera hai <br />
+          Roshan roshan aakhon main sapnon ka saagar <br />
+          Jismain prem sitaron ki chaadar jaise jhalak rahi hai
+        </p>
+  
+        <p>
+          Lahron lahron baat kare to jaise moti barse <br />
+          Jaise kahin chandi ki payal goonjey <br />
+          Jaise kahin sheeshe ke jaam girey <br />
+          Aur chhann se tootey jaise koi chhip ke sitaar bajaye
+        </p>
+  
+        <p>
+          Jaise koi chaandni raat main gaye <br />
+          Jaise koi hole se paas bulaye <br />
+          Kaisi meethi baatain thee vo <br />
+          Kaisi mulakaatein thee vo
+        </p>
+  
+        <p>
+          Jab maine jaana tha <br />
+          Nazron se kaise pighalte hain dil <br />
+          Aur aarzoo paati hai kaise manzil <br />
+          Aur kaise utarta hai chaand jameen par
+        </p>
+  
+        <p>
+          Kaise kabhi lagta hai swarg agar hai <br />
+          To bas hai yahin par
+        </p>
+  
+        <p>
+          Usne bulaya mujhe, aur samjhaya mujhe <br />
+          Hum jo mile hain, hamain aise hi milna tha <br />
+          Gul jo khile hain, unhe aise hi khilna tha
+        </p>
+  
+        <p>
+          Janmo ke bandhan, janmo ke rishtey hain <br />
+          Jab bhi hum janme to hum yuhin milte hain
+        </p>
+  
+        <p>
+          Kaanon main mere jaise shahed sa ghulne lage <br />
+          Khwaabon ke dar jaise aakhon main khulne lage <br />
+          Khwaabon ki duniya bhi kitni haseen aur <br />
+          Kaisi rangeen thee khwaabon ki duniya
+        </p>
+  
+        <p>
+          Jo kahne ko thee par kahin bhi nahi thee <br />
+          Khwaab jo toote mere, aakh jo khuli meri <br />
+          Hosh jo aaya mujhe <br />
+          Maine dekha, maine jaana
+        </p>
+  
+        <p>
+          Vo jo kabhi aaya tha, nazron pe chhaya tha <br />
+          Dil main samaya tha, ja bhi chuka hai <br />
+          Aur dil mera hai ab tanha tanha
+        </p>
+  
+        <p>
+          Na to koi armaan hai, na koi tamanna hai <br />
+          Aur na koi sapna hai
+        </p>
+  
+        <p>
+          Ab jo mere din aur ab jo meri ratain hain <br />
+          Unmain sirf aansoon hain <br />
+          Unmain sirf dard ki ranj ki baatein hain <br />
+          Aur pharyaadein hain
+        </p>
+  
+        <p>
+          Mera ab bhi koi nahi, main hoon aur khoye hue <br />
+          Pyaar ki yaadein hain <br />
+          Pyaar ki yaadein hain <br />
+          Pyaar ki yaadein hain
+        </p>
+  
+        <p className="text-center font-semibold mt-6">
+          || Breathless - Song Ends ||
+        </p>
+      </div>
+    );
+  }
+  
